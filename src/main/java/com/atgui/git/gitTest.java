62e1d3f3bbdf0a287c2d0d60d444efcff0067a50
cjw <1924327012@qq.com>
2022-06-27 01:13:27 +0800

    idea git thrid commit

--- src/main/java/com/atgui/git/gitTest.java
+++ src/main/java/com/atgui/git/gitTest.java
@@ -7,6 +7,6 @@ public class gitTest {
         System.out.println("hello git 11111111");
         System.out.println("hello git 22222222");
         System.out.println("hello git 33333333");
-        System.out.println("hello git 44444444");
+        System.out.println("hello git 55555555");
     }
 }
