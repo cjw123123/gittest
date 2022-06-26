caf85e5f3899d0de2674cc026d298ce6f08b5776
cjw <1924327012@qq.com>
2022-06-27 01:12:36 +0800

    idea git second commit

--- src/main/java/com/atgui/git/gitTest.java
+++ src/main/java/com/atgui/git/gitTest.java
@@ -4,5 +4,7 @@ public class gitTest {
 
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("hello git 11111111");
+        System.out.println("hello git 22222222");
     }
 }
