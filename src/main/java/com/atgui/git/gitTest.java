77f85bf08b617ce0fee489f5ae3163685dcacb8f
cjw <1924327012@qq.com>
2022-06-27 20:47:57 +0800

    hot-fix thrid commit

--- src/main/java/com/atgui/git/gitTest.java
+++ src/main/java/com/atgui/git/gitTest.java
@@ -5,6 +5,6 @@ public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git 66666666");
-        System.out.println("hello git 77777777");
+        System.out.println("master----->");
     }
 }
